7650e3df0ed13a8b3fd11ce4e64e1a917167b5a6
Vaibhav Singh <vaibhav.singh@exiverlabs.co.in>
2025-03-11 14:24:55 +0530

    Removed Google Chart API & included QuickCharts.io

--- includes/qr-generator.php
+++ includes/qr-generator.php
@@ -3,7 +3,7 @@ if (!defined('ABSPATH')) exit;
 
 function wqr_generate_qr($id, $type = 'product') {
     $data = ($type === 'product') ? get_permalink($id) : wc_get_order($id)->get_view_order_url();
-    return "https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=" . urlencode($data) . "&choe=UTF-8";
+    return "https://quickchart.io/qr?text=" . urlencode($data) . "&size=500x500&margin=10";
 }
 
 // QR Code Download as PNG
@@ -12,6 +12,7 @@ if (isset($_GET['wqr_download'])) {
     $type = ($_GET['type'] === 'order') ? 'order' : 'product';
     $qr_url = wqr_generate_qr($id, $type);
 
+    // Fetch QR code image and serve as a PNG download
     $image = file_get_contents($qr_url);
     header('Content-Type: image/png');
     header('Content-Disposition: attachment; filename="QR_' . $type . '_' . $id . '.png"');
